355b2397ed017d27b3c0cbc5a7b2ec9b844e61e8
Abdullah Moyeen <mcc.moyeen@gmail.com>
2016-04-16 19:20:28 -0500

    modified GroupModel and added UserGroupModel

---
+++ src/main/java/com/viiup/web/flock/models/UserGroupModel.java
@@ -0,0 +1,27 @@
+package com.viiup.web.flock.models;
+
+/**
+ * Created by AbdullahMoyeen on 4/14/16.
+ */
+public class UserGroupModel {
+
+    private int userId;
+    private boolean isMember;
+    public GroupModel group;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void setMember(boolean member) {
+        isMember = member;
+    }
+}
